a97cfbd2b0d09c44e659f342e906ff03fcd83453
unknown <IamMerove>
2026-03-06 16:27:45 +0400

    encore

--- app_api/maths/__init__.py
+++ app_api/maths/__init__.py
@@ -1,6 +1,3 @@
 # maths/__init__.py
 
-# Ce fichier marque le dossier comme un package Python
-# Tu peux laisser vide, mais pour plus de sécurité on met un commentaire
-
-from .mon_module import add, sub, square, print_data  # ← optionnel : expose les fonctions
+__version__ = "0.1.0"
